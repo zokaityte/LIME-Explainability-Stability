310e3adb2d3e4e90cf5d3ef797fd78a73b099d66
gintare.zokaityte <gintarezok@gmail.com>
2024-10-12 13:51:46 +0300

    Update main tests

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from sklearn.datasets import make_classification
 from sklearn.ensemble import RandomForestClassifier
 
 from lime.lime_tabular import LimeTabularExplainer
-from sklearn.preprocessing import MinMaxScaler
 
 def test_lime_tabular_explainer_3_classes():
     # Generate classification data
@@ -14,11 +13,10 @@ def test_lime_tabular_explainer_3_classes():
 
     # Explain
     explainer = LimeTabularExplainer(x_raw, kernel_width=3, random_state=42, sample_around_instance=False)
-    # TODO investigate the effect of sample_around_instance. Sample around instance is set to default False
-    explanation = explainer.explain_instance(x_raw[0, :], clf.predict_proba, sampling_method='gaussian')
+    explanation = explainer.explain_instance(x_raw[0, :], clf.predict_proba)
 
-    # Test if explanation is correct
-    assert explanation.as_list() == [('3', 0.043831946522315667), ('2', -0.043577987862162285),
+    # Test if explanation is correct for the first class
+    assert explanation.as_list(1) == [('3', 0.043831946522315667), ('2', -0.043577987862162285),
                                      ('0', 0.03887729586806089), ('4', 0.019489294650031276),
                                      ('1', -0.014745938524001936)]
 
@@ -67,5 +65,5 @@ def test_lime_tabular_explainer_2_classes():
 
 
 if __name__ == '__main__':
-    # test_lime_tabular_explainer_3_classes()
+    test_lime_tabular_explainer_3_classes()
     test_lime_tabular_explainer_2_classes()
